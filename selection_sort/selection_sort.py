63e9402e1fb4f68b2c5776a465950d00d19daff5
Issa Mohamed <issa.mohamed@student.moringaschool.com>
2025-06-20 12:14:22 +0300

    Completed selection sort challenge

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,13 @@
 def selection_sort(arr):
     # TODO: Implement selection sort
+    arr_length = len(arr)
+    for i in range(arr_length  -1):
+        min_index = i
+        for j in range(i+1, arr_length):
+            if arr[j]<arr[min_index]:
+                min_index = j
+
+        arr[i],arr[min_index]=arr[min_index],arr[i]
+    return arr
+
     pass
